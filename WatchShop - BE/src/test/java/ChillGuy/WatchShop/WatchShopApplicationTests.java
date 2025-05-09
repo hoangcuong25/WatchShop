a4026eef777c3cf35974f53ef4f900a5d0977e9a
hoang cuong <vancuong25112003@gmail.com>
2025-05-09 13:28:10 +0700

    update

---
+++ WatchShop - BE/src/test/java/ChillGuy/WatchShop/WatchShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ChillGuy.WatchShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
