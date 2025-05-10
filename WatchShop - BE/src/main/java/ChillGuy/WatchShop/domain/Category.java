1070f61bf05cd7e47d769a2b3faf4784e32e4466
hoang cuong <vancuong25112003@gmail.com>
2025-05-11 03:16:20 +0700

    category

--- WatchShop - BE/src/main/java/ChillGuy/WatchShop/domain/Category.java
+++ WatchShop - BE/src/main/java/ChillGuy/WatchShop/domain/Category.java
@@ -7,54 +7,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "products")
+@Table(name = "categories")
 @Getter
 @Setter
-public class Product {
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "Name không được để trống")
     private String name;
-
-    @NotBlank(message = "Description không được để trống")
-    private String description;
-
-    @NotBlank(message = "Old Price không được để trống")
-    private String oldPrice;
-
-    @NotBlank(message = "New Price không được để trống")
-    private String newPrice;
-
-    @NotBlank(message = "Discount không được để trống")
-    private String discount;
-
-    private Integer stockQuantity;
-
-    @NotBlank(message = "Status không được để trống")
-    private String status;
-
-    @NotNull(message = "Brand không được để trống")
-    @ManyToOne(fetch = jakarta.persistence.FetchType.LAZY)
-    @JoinColumn(name = "brand_id", nullable = false)
-    private Brand brand;
-
-    @NotNull(message = "Machine Type không được để trống")
-    @ManyToOne(fetch = jakarta.persistence.FetchType.LAZY)
-    @JoinColumn(name = "machine_type_id", nullable = false)
-    private MachineType machineType;
 
     private Instant createdAt;
     private Instant updatedAt;
--- WatchShop - BE/src/main/java/ChillGuy/WatchShop/domain/MachineType.java
+++ WatchShop - BE/src/main/java/ChillGuy/WatchShop/domain/MachineType.java
@@ -1,15 +1,19 @@
 package ChillGuy.WatchShop.domain;
 
 import java.util.List;
+import java.time.Instant;
 import java.util.ArrayList;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import ChillGuy.WatchShop.util.SecurityUtil;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -30,4 +34,27 @@ public class MachineType {
     @OneToMany(mappedBy = "machineType", fetch = jakarta.persistence.FetchType.LAZY)
     @JsonIgnore
     private List<Product> products = new ArrayList<>();
+
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    @PrePersist
+    public void handleBeforeCreate() {
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void handleBeforeUpdate() {
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+
+        this.updatedAt = Instant.now();
+    }
 }
